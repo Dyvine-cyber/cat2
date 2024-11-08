044a3b0a8ba0ad1c9905981853cb0dc2bb02bbae
Dyvine-cyber <dyvineeshiuma20@gmail.com>
2024-11-08 20:58:14 +0300

    Add files via upload

---
+++ cat2 question 2.c
@@ -0,0 +1,17 @@
+//A program of 2D arrray
+#include<stdio.h>
+int main() {
+//Declaring a 2D array 
+int score[2][2]= {{65,92},{84,72}};
+{
+for(int i=0;i<2;i++){
+
+for(int j=0;j<2;j++){
+//printing each element in 2D array 
+printf("%d",scores[i][j]);
+
+}
+printf("\n");
+}
+return 0;
+}
---
+++ cat2 question3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+float hours_worked, hourly_wage, gross_pay, tax, net_pay;
+    
+    // Get user input
+	printf("Enter hours worked in a week: ");
+    scanf("%f", &hours_worked);
+    printf("Enter hourly wage: ");
+    scanf("%f", &hourly_wage);
+
+    // Calculate gross pay
+    if (hours_worked > 40) {
+        gross_pay = (40 * hourly_wage) + ((hours_worked - 40) * hourly_wage * 1.5);
+    } else {
+        gross_pay = hours_worked * hourly_wage;
+    }
+
+    // Calculate tax
+    if (gross_pay <= 600) {
+        tax = gross_pay * 0.15;
+    } else {
+        tax = (600 * 0.15) + ((gross_pay - 600) * 0.20);
+    }
+
+    // Calculate net pay
+    net_pay = gross_pay - tax;
+
+    // Print the results
+    printf("Gross Pay: $%.2f\n", gross_pay);
+    printf("Taxes: $%.2f\n", tax);
+   printf("Net Pay: $%.2f\n", net_pay);
+return 0;
+}
